a315759f2bb564c47630dda4e2f433cf6204bd54
uladzislaubrel <uladzislau.brel@innowise.com>
2026-02-24 13:53:37 +0100

    Loading passengers procedure was changed

--- Task_6_Snowflake/dags/sql/procedures/silver/sp_load_dim_passengers_scd2.sql
+++ Task_6_Snowflake/dags/sql/procedures/silver/sp_load_dim_passengers_scd2.sql
@@ -1,83 +1,87 @@
-CREATE OR REPLACE PROCEDURE AIRLINE_DWH.SILVER_DATA.SP_LOAD_DIM_PASSENGERS_SCD2()
+CREATE OR REPLACE PROCEDURE AIRLINE_DWH.SILVER_DATA.SP_LOAD_DIM_PASSENGERS_SCD2_MERGE()
 RETURNS VARCHAR
 LANGUAGE SQL
 EXECUTE AS CALLER
 AS
 $$
 DECLARE
-    rows_inserted INT := 0;
-    rows_updated INT := 0;
+    rows_affected INT := 0;
+    run_timestamp TIMESTAMP := CURRENT_TIMESTAMP();
 BEGIN
     BEGIN TRANSACTION;
-    -- 1. if there is not new data - doing nothing
+
+    -- 1. If there is no data - exit
     IF (SYSTEM$STREAM_HAS_DATA('AIRLINE_DWH.RAW_DATA.STREAM_FOR_PASSENGERS') = FALSE) THEN
         COMMIT;
         RETURN 'No new data found';
     END IF;
 
-    -- 2. create new temp table (STAGING)
-    -- immediately calculate new hash for new raws
-    CREATE OR REPLACE TEMPORARY TABLE T_PASSENGERS_STG AS
-    SELECT DISTINCT
-        passenger_id,
-        first_name,
-        last_name,
-        gender,
-        TRY_TO_NUMBER(age) as age,
-        nationality,
-        -- calculate hash of important/changeable colimns
-        MD5(CONCAT(
-            IFNULL(first_name, ''), 
-            IFNULL(last_name, ''), 
-            IFNULL(nationality, '')
-        )) as new_row_hash,
-        ingestion_time
-    FROM AIRLINE_DWH.RAW_DATA.STREAM_FOR_PASSENGERS
-    WHERE METADATA$ACTION = 'INSERT';
+    -- 2. MERGE
+    MERGE INTO AIRLINE_DWH.SILVER_DATA.DIM_PASSENGERS T
+    USING (
+        WITH STREAM_DATA AS (
+            SELECT DISTINCT
+                passenger_id,
+                first_name,
+                last_name,
+                gender,
+                TRY_TO_NUMBER(age) AS age,
+                nationality,
+                HASH(first_name, last_name, nationality) AS new_row_hash
+            FROM AIRLINE_DWH.RAW_DATA.STREAM_FOR_PASSENGERS
+            WHERE METADATA$ACTION = 'INSERT'
+        )
+        
+        -- Part 1 - rows for update(were changed recently)
+        SELECT 
+            src.passenger_id AS merge_key, -- ID for compare with Target
+            src.*
+        FROM STREAM_DATA src
+        JOIN AIRLINE_DWH.SILVER_DATA.DIM_PASSENGERS tgt 
+          ON src.passenger_id = tgt.passenger_id 
+         AND tgt.is_current = TRUE 
+         AND src.new_row_hash <> tgt.row_hash
 
-    -- 3. step 1 (UPDATE): close old raws if data has updated
-    -- If ID is similar, but hashes are different- it means person updated his data
-    UPDATE AIRLINE_DWH.SILVER_DATA.DIM_PASSENGERS T
-    SET 
-        is_current = FALSE,
-        valid_to = CURRENT_TIMESTAMP()
-    FROM T_PASSENGERS_STG S
-    WHERE T.passenger_id = S.passenger_id   -- same person
-      AND T.is_current = TRUE               -- last current version(raw)
-      AND T.row_hash <> S.new_row_hash;     -- comparasing hashes
+        UNION ALL
 
-    rows_updated := SQLROWCOUNT; -- write number of update
+        -- Part 2 - Rows for INSERT (new + new versions of updated rows)
+        SELECT 
+            NULL AS merge_key, -- synthetic NULL, for MERGE doesn't find coincidence
+            src.*
+        FROM STREAM_DATA src
+    ) S
+    ON T.passenger_id = S.merge_key -- match targer and source
 
-    -- 4. step 2 (INSERT): 
-    -- insert, if:
-    -- a) There wasn't this person(new)
-    -- b) Person was here, but he was closed recently(updated)
-    INSERT INTO AIRLINE_DWH.SILVER_DATA.DIM_PASSENGERS (
-        passenger_id, first_name, last_name, gender, age, nationality, 
-        row_hash, valid_from, valid_to, is_current
-    )
-    SELECT 
-        S.passenger_id, S.first_name, S.last_name, S.gender, S.age, S.nationality,
-        S.new_row_hash,      -- write hash to base
-        CURRENT_TIMESTAMP(), -- actions from now
-        NULL,                -- action for eternal
-        TRUE                 -- actual
-    FROM T_PASSENGERS_STG S
-    LEFT JOIN AIRLINE_DWH.SILVER_DATA.DIM_PASSENGERS T 
-        ON S.passenger_id = T.passenger_id 
-        AND T.is_current = TRUE
-        AND T.row_hash = S.new_row_hash -- looking for complete match
-    WHERE T.passenger_id IS NULL; -- insert just these, which doesn't found(either completely new, or updated)
+    -- Step 1: If key matches (part 1) -> close old row
+    WHEN MATCHED THEN
+        UPDATE SET 
+            T.is_current = FALSE,
+            T.valid_to = :run_timestamp
 
-    rows_inserted := SQLROWCOUNT;
+    -- Step 2: If key doesn't match(part 2, bc of merge_key = NULL) -> insert new row
+    WHEN NOT MATCHED THEN
+        INSERT (
+            passenger_id, first_name, last_name, gender, age, nationality, 
+            row_hash, valid_from, valid_to, is_current
+        )
+        VALUES (
+            S.passenger_id, S.first_name, S.last_name, S.gender, S.age, S.nationality,
+            S.new_row_hash, 
+            :run_timestamp, 
+            NULL, 
+            TRUE
+        );
 
-    -- 5. logging
+    -- 3. number of affected rows (update+ insert)
+    rows_affected := SQLROWCOUNT;
+
+    -- 4. Логируем
     INSERT INTO AIRLINE_DWH.UTILS.ETL_LOGS (process_name, layer, rows_affected, status, error_message)
-    VALUES ('SP_LOAD_DIM_PASSENGERS_SCD2', 'SILVER', :rows_inserted + :rows_updated, 'SUCCESS', 
-            'Inserted: ' || :rows_inserted || ', Updated (Closed): ' || :rows_updated);
+    VALUES ('SP_LOAD_DIM_PASSENGERS_SCD2_MERGE', 'SILVER', :rows_affected, 'SUCCESS', 
+            'Total rows affected (Merged): ' || :rows_affected);
+
     COMMIT;
-    RETURN 'Done. Inserted: ' || :rows_inserted || ', Updated: ' || :rows_updated;
+    RETURN 'Done. Total rows affected: ' || :rows_affected;
 
 END;
 $$;
-
